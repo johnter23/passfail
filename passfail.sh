8fd186caf368645f093f327c8248574851e49563
johnter23 <50182408+johnter23@users.noreply.github.com>
2019-06-17 15:27:11 -0500

    Update passfail.sh

--- passfail.sh
+++ passfail.sh
@@ -3,4 +3,4 @@
 DATE=`date +"%Y-%m-%d-%H%M"`
 WRAPPER_LOCATION=$1
 
-/usr/bin/java -jar --add-modules java.se.ee $WRAPPER_LOCATION -action passfail -vid 061aff34013705950b50526883bb011f -vkey 716a42dceeb05e9305da7bf52db4bab712bc507943f0aecb5aa21faa81b3806f7a0aae8e992b5d729620d447ff0b58dcd105f97f1cc205454f59bfb6e0af74ff -appname "Pivotal PoC"
+/usr/bin/java -jar --add-modules /hmswork/VeracodeJavaAPI.jar $WRAPPER_LOCATION -action passfail -vid 061aff34013705950b50526883bb011f -vkey 716a42dceeb05e9305da7bf52db4bab712bc507943f0aecb5aa21faa81b3806f7a0aae8e992b5d729620d447ff0b58dcd105f97f1cc205454f59bfb6e0af74ff -appname "Pivotal PoC"
