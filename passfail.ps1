d76353644f6ef35b1945c5763b6eedf3defd9418
johnter23 <50182408+johnter23@users.noreply.github.com>
2019-06-18 06:01:24 -0500

    Update passfail.ps1

--- passfail.ps1
+++ passfail.ps1
@@ -5,8 +5,8 @@ foreach($obj in $Passfail)
 if($obj.PASSFAIL -eq "Pass")
 {
 Write-Host "I will pause this pipeline!!!"
-##fly login -t con -c http://concourse.hms.hmsy.com:8080 -u admin -p admin
-##fly pause-pipeline --pipeline verapassfail01 -t con
+fly login -t con -c http://concourse.hms.hmsy.com:8080 -u admin -p admin
+fly pause-pipeline --pipeline verapassfail01 -t con
 }
 else
 {
