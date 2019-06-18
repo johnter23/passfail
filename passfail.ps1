7c6be4ea140e00ae2db9e9a61892be800731ce42
johnter23 <50182408+johnter23@users.noreply.github.com>
2019-06-18 08:36:25 -0500

    Update passfail.ps1

--- passfail.ps1
+++ passfail.ps1
@@ -4,12 +4,14 @@ foreach($obj in $Passfail)
 {
 if($obj.PASSFAIL -eq "Pass")
 {
-Write-Host "I will pause this pipeline!!!"
-fly login -t con -c http://concourse.hms.hmsy.com:8080 -u admin -p admin
-fly pause-pipeline --pipeline verapassfail01 -t con
+Write-Host "Code is Compliant. Continue Pipeline"
+
 }
 else
 {
-write-host "Test Didn't Pass"
+write-host "Code Not Compliant!!! Pause Pipeline"
+fly login -t con -c http://concourse.hms.hmsy.com:8080 -u admin -p admin
+fly pause-pipeline --pipeline verapassfail01 -t con
+
 }
 }
