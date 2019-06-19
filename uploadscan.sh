5f7967d90047286da12f380d13f0b8d421bd6e16
deeprathna <root@lspcf001.hms.hmsy.com>
2019-06-19 14:57:39 -0500

    On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage) 	new file:   uploadscan.sh

---
+++ uploadscan.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+DATE=`date +"%Y-%m-%d-%H%M"`
+WRAPPER_LOCATION=$1
+
+/usr/bin/java -jar /hmswork/VeracodeJavaAPI.jar -action uploadandscan -vid 061aff34013705950b50526883bb011f -vkey 716a42dceeb05e9305da7bf52db4bab712bc507943f0aecb5aa21faa81b3806f7a0aae8e992b5d729620d447ff0b58dcd105f97f1cc205454f59bfb6e0af74ff -appname "Pivotal PoC1" -version "1.0" -filepath "artifact-dir/*.jar"
+
